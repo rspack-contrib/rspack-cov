935d51c0023d23194e494e0e2b2ce3ea99135364
Mihir Karbelkar <karbelkar.mihir@gmail.com>
2023-12-11 14:29:40 +0530

    Add vercel analytics

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,5 +1,8 @@
 <script>
+	import { dev } from '$app/environment';
+	import { inject } from '@vercel/analytics';
 	import '../app.css';
+	inject({ mode: dev ? 'development' : 'production' });
 </script>
 
 <main>
